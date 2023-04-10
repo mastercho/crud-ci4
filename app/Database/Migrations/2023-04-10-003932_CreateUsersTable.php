a1d66ff3a05faf5599cf52688cf85fdb6882a182
mastercho <souljanboy@gmail.com>
2023-04-10 03:58:51 +0300

    Added migration for user table

--- app/Database/Migrations/2023-04-10-003932_CreateUsersTable.php
+++ app/Database/Migrations/2023-04-10-003932_CreateUsersTable.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace App\Database\Migrations;
+<?php namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
@@ -8,11 +6,55 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'firstname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'lastname' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'unique' => true,
+            ],
+            'mobile' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
